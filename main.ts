0b4ce6c2de4c5a543953d4cacf7b6d065010043d
lbaitemple <lbai@temple.edu>
2022-02-28 16:59:44 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -35,7 +35,7 @@ enum NeoPixelMode {
     //% block="RGB (RGB format)"
     RGB_RGB = 3
 }
-//% weight=20 color=#0855AA icon="\uf109" block="WS2812b"
+
 namespace ws2812b {
     //% block="send buffer"
     //% block="send buffer $buf at Digital PIN $pin "
@@ -47,4 +47,41 @@ namespace ws2812b {
     export function setBufferMode(pin: DigitalInOutPin, mode: number) {
 
     }
+}
+//% weight=20 color=#0855AA icon="\uf109" block="NeoLED"
+namespace NeoLED {
+         /**
+         * Send all the changes to the strip.
+         */
+        //% block="send RGB $rgb at Digital PIN $pin "
+      export function show(rgb: number, pin: DigitalInOutPin) {
+            // only supported in beta
+            // ws2812b.setBufferMode(this.pin, this._mode);
+            this.setPixelRGB(rgb);
+            ws2812b.sendBuffer(this.buf, pin);
+     }
+
+    
+     private setPixelRGB(rgb: number): void {
+
+            let stride = this._mode === NeoPixelMode.RGBW ? 4 : 3;
+
+            let red = unpackR(rgb);
+            let green = unpackG(rgb);
+            let blue = unpackB(rgb);
+
+            let br = 128;
+            if (br < 255) {
+                red = (red * br) >> 8;
+                green = (green * br) >> 8;
+                blue = (blue * br) >> 8;
+            }
+            this.setBufferRGB(red, green, blue)
+        }
+        private setBufferRGB(red: number, green: number, blue: number): void {
+            this.buf[offset + 0] = red;
+            this.buf[offset + 1] = green;
+            this.buf[offset + 2] = blue;
+        }
+
 }
