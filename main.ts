ff43f10d739b38d68cb1125d3c27be1ba2d054a0
lbaitemple <lbai@temple.edu>
2022-02-28 16:35:04 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 //% weight=20 color=#0855AA icon="\uf109" block="WS2812b"
 namespace ws2812b {
+    //% block="send buffer"
     //% shim=sendBufferAsm
     export function sendBuffer(buf: Buffer, pin: DigitalInOutPin) {
     }
 
+     //% block="send buffermode"
     //% shim=setBufferMode
     export function setBufferMode(pin: DigitalInOutPin, mode: number) {
 
