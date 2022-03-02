44c96f966c9281ace8bc2c25a5c991fe51f3f256
lbaitemple <lbai@temple.edu>
2022-03-01 19:43:36 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 namespace ws2812b {
     //% shim=sendBufferAsm
-    export function sendBuffer(buf: Buffer, pin: DigitalInOutPin) {
+    export function sendBuffer(buf: Buffer, pin: DigitalPin) {
     }
 
     //% shim=setBufferMode
-    export function setBufferMode(pin: DigitalInOutPin, mode: number) {
+    export function setBufferMode(pin: DigitalPin, mode: number) {
 
     }
 }
