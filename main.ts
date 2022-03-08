2dd91503832d60824ff9b79dda93742c3b90c5d1
lbaitemple <lbai@temple.edu>
2022-03-08 18:13:14 -0500

    Update pxt.json, main.py, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,1 @@
-namespace ws2812b {
-    //% shim=setBufferMode
-    export function setBufferMode(pin: DigitalInOutPin, mode: number) {
-
-    }
-    //% shim=sendBufferAsm
-    export function sendBuffer(buf: Buffer, pin: DigitalInOutPin) {
-    }
-
-}
-
 
