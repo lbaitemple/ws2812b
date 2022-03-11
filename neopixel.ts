bdc78f54b226f2118cfe5078d065b467098e55cf
lbaitemple <lbai@temple.edu>
2022-03-10 21:35:12 -0500

    Update neopixel.ts

--- neopixel.ts
+++ neopixel.ts
@@ -1172,9 +1172,12 @@ namespace light {
 
 
         const strip = new NeoPixelStrip();
+    
         strip._mode = mode;
         strip._length = Math.max(0, numleds | 0);
-        strip._dataPin = pins.P0;
+        strip._dataPin = pin;    
+        strip._buf = pins.createBuffer(numleds * strip._mode);
+        strip.setBrightness(128);
         if (strip._dataPin) // board with no-board LEDs won't have a default pin
             strip._dataPin.digitalWrite(false);
         
